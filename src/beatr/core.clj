fb9ca791315c429aab46f36ecf9fe80947290025
Roger Allen <rallen@gmail.com>
2013-09-30 23:29:39 -0700

    Add Quil to visualize the grid of sound as it plays.

--- src/beatr/core.clj
+++ src/beatr/core.clj
@@ -1,5 +1,6 @@
 (ns beatr.core
-  (:require [overtone.live :as o]))
+  (:require [overtone.live :as o])
+  (:require [quil.core :as q]))
 
 ;; based on overtone/src/overtone/examples/timing/interal_sequencer.clj
 ;; code rearranged to fit my brain + add polyrhythm
@@ -24,7 +25,9 @@
 ;; put global metronome count on root-cnt-bus
 (defonce root-cnt-bus (o/control-bus))
 (o/defsynth root-cnt-synth []
-  (o/out:kr root-cnt-bus (o/pulse-count:kr (o/in:kr root-trg-bus))))
+  (let [cur-count (o/pulse-count:kr (o/in:kr root-trg-bus))]
+    (o/tap "cur-root-cnt" 30 cur-count)
+    (o/out:kr root-cnt-bus cur-count)))
 
 (defn dovec [xs]
   "doall and convert sequence to vector"
@@ -72,10 +75,13 @@
 (defonce root-cnt-atom (atom nil)) ; will contain the synth driving the root-cnt-bus
 (defonce beat-trgs-atom (atom [])) ; will contain a list of synths driving the beat-trg-buses
 (defonce beat-cnts-atom (atom [])) ; will contain a list of synths driving the beat-cnt-buses
+(defonce seq-secs-atom (atom nil)) ; length of the sequences in seconds
+(defonce seq-beats-atom (atom [])) ; beats in each sequence
 ;; list of o/buffers.  one for each sequence. buffers are the length
 ;; of the corresponding sequence and contain either 1 or 0 to control
 ;; playing the sample on that beat.  Initialized to be all 0
 (defonce seq-bufs-atom (atom []))
+(defonce seq-vecs-atom (atom []))  ; copy of bufs for client use
 ;; a vector of vectors of synths.  each sequence contatains one synth
 ;; per beat that plays the sample
 (defonce synth-seqs-atom (atom []))
@@ -85,6 +91,10 @@
 ;;   restart
 ;;   set-seq-ctl
 ;;   set-seq-buf
+(defn get-root-cnt []
+  @(get-in @root-cnt-atom [:taps "cur-root-cnt"]))
+;;(get-root-cnt)
+
 (defn restart [seq-seconds
                beat-seq-beats
                beat-seq-synths]
@@ -93,6 +103,8 @@
     (assert (>= MAX-BEAT-BUSES (count beat-set)))
     (assert (= (count beat-seq-beats) (count beat-seq-synths)))
 
+    (swap! seq-secs-atom (fn [_] seq-seconds))
+    (swap! seq-beats-atom (fn [_] beat-seq-beats))
     (swap! root-trg-atom (fn [_] (root-trg-synth)))
     (swap! root-cnt-atom (fn [_] (root-cnt-synth)))
     (swap! beat-trgs-atom
@@ -109,6 +121,9 @@
     (swap! seq-bufs-atom
            (fn [_] (dovec (for [num-beats beat-seq-beats]
                            (o/buffer num-beats)))))
+    (swap! seq-vecs-atom
+           (fn [_] (dovec (for [num-beats beat-seq-beats]
+                           (apply vector (repeat num-beats 0))))))
 
     (swap! synth-seqs-atom
            (fn [_] (dovec (for [[seq-index num-beats] (map-indexed vector beat-seq-beats)]
@@ -136,6 +151,8 @@
 (defn set-seq-buf
   [seq-index new-buf]
   (o/buffer-write! (nth @seq-bufs-atom seq-index) new-buf)
+  (swap! seq-vecs-atom (fn [xs]
+                         (assoc xs seq-index new-buf)))
   nil)
 
 ;; ======================================================================
@@ -183,14 +200,13 @@
     (set-seq-buf 3 [0 0 1 1 0 0 1 1])
     )
 
-
   ;; adjust tempo via
-  (o/ctl @root-trg-atom :rate 208)
+  (o/ctl @root-trg-atom :rate 130)
 
   ;; adjust sound via
   (set-seq-ctl 1 :sample-buf clap)
-  (set-seq-ctl 1 :amp 0.5)
-  (set-seq-ctl 1 :rate 1.5)
+  (set-seq-ctl 1 :amp 0.25)
+  (set-seq-ctl 1 :sample-rate 1.5)
 
 )
 
@@ -218,4 +234,109 @@
     )
 
   (o/stop)
+
+  (get-root-cnt)
+)
+
+;; ======================================================================
+;; quil for seeing what it looks like
+
+(def colors {:background [238 173  14]
+             :grid       [ 20  20  20]
+             :note       [100 100 100]
+             :play       [238 238 238]
+             :time       [ 14 173 238]})
+(def layout {:grid-offset  40
+             :grid-width  400
+             :cell-height  40
+             :note-scale  0.7})
+
+(defn setup []
+  (q/smooth)
+  (q/frame-rate 20))
+
+(defn draw-grid []
+  (apply q/stroke (colors :grid))
+  (q/stroke-weight 1.5)
+  (q/push-matrix)
+  (q/translate (layout :grid-offset) (layout :grid-offset))
+  (q/line 0 0 (layout :grid-width) 0)
+  (doall
+   (dotimes [i (count @seq-beats-atom)]
+     (q/line 0 (layout :cell-height) (layout :grid-width) (layout :cell-height))
+     (q/line 0 0 0 (layout :cell-height))
+     (let [num-beats (@seq-beats-atom i)
+           cell-width (/ (layout :grid-width) num-beats)]
+       (doall
+        (dotimes [j num-beats]
+          (q/line (* (inc j) cell-width) 0 (* (inc j) cell-width) (layout :cell-height)))))
+     (q/translate 0 (layout :cell-height))))
+  (q/pop-matrix))
+
+(defn draw-note-highlights []
+  (apply q/stroke (colors :play))
+  (q/stroke-weight 0.0)
+  (apply q/fill (colors :play))
+  (q/push-matrix)
+  (q/translate (layout :grid-offset) (layout :grid-offset))
+  (let [cur-time (get-root-cnt)
+        grid-max-time (* DEFAULT-ROOT-RATE @seq-secs-atom)
+        time-x (/ (mod cur-time grid-max-time) grid-max-time)
+        playing-cells (apply vector (map #(int (* time-x %)) @seq-beats-atom))]
+   (doall
+    (dotimes [i (count @seq-beats-atom)]
+      (let [num-beats (@seq-beats-atom i)
+            cell-width (/ (layout :grid-width) num-beats)
+            j (playing-cells i)]
+        (q/rect (* j cell-width) 0 cell-width (layout :cell-height)))
+      (q/translate 0 (layout :cell-height)))))
+  (q/pop-matrix))
+
+(defn draw-notes []
+  (apply q/stroke (colors :note))
+  (q/stroke-weight 0.0)
+  (apply q/fill (colors :note))
+  (q/push-matrix)
+  (q/translate (layout :grid-offset) (layout :grid-offset))
+  (doall
+   (dotimes [i (count @seq-beats-atom)]
+     (let [num-beats (@seq-beats-atom i)
+           cell-width (/ (layout :grid-width) num-beats)
+           note-width (* (layout :note-scale) cell-width)
+           note-height (* (layout :note-scale) (layout :cell-height))
+           note-size (min note-width note-height)]
+       (doall
+        (dotimes [j num-beats]
+          (when (= 1 ((@seq-vecs-atom i) j))
+            (q/ellipse (+ (* 0.5 cell-width) (* j cell-width)) (* 0.5 (layout :cell-height))
+                       note-size note-size)))))
+     (q/translate 0 (layout :cell-height))))
+  (q/pop-matrix))
+
+(defn draw-time []
+  (let [cur-time (get-root-cnt)
+        grid-max-time (* DEFAULT-ROOT-RATE @seq-secs-atom)
+        time-x (* (/ (mod cur-time grid-max-time) grid-max-time)
+                  (layout :grid-width))
+        time-x (+ time-x (layout :grid-offset))]
+  (apply q/stroke (colors :time))
+  (q/stroke-weight 0.5)
+  (q/line time-x (- (layout :grid-offset) 10)
+          time-x (+ (* (layout :cell-height) (inc (count @seq-beats-atom))) 10))))
+
+(defn draw []
+  (apply q/background (colors :background))
+  (draw-note-highlights)
+  (draw-grid)
+  (draw-notes)
+  (draw-time)
+  )
+
+(defn run []
+  (q/defsketch doodle :title "beatr" :setup setup :draw draw :size [600 600])
+  nil)
+
+;; see it go!
+(comment
+  (run)
 )
